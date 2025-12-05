b77a68c684158873b5a9ab428789514cd1fdb6f0
misha <medinskijmisa228@gmail.com>
2025-12-05 09:56:03 +0100

    create config.py file. This file read enviroment settings from .env file

---
+++ backend/app/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+    postgres_url: str
+    
