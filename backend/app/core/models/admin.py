e182aed6e80c1048f138488c3b4e5089e3100b8d
misha <medinskijmisa228@gmail.com>
2025-12-23 12:39:33 +0100

    Added unique in username field Admin model

--- backend/app/core/models/admin.py
+++ backend/app/core/models/admin.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from .base import Base
 
 class Admin(Base):
-    username: Mapped[str] = mapped_column(String(255), nullable=False, index=True)
+    username: Mapped[str] = mapped_column(String(255), nullable=False, index=True, unique=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     last_login_at: Mapped[datetime] = mapped_column(DateTime, nullable=True, default=None)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
