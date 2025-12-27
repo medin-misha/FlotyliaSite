5234ba37023490041e1175d38033bb422a2bd0fd
misha <medinskijmisa228@gmail.com>
2025-12-27 11:41:49 +0100

    added health check endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,5 +29,10 @@ app.add_middleware(
 )
 app.include_router(main_router)
 
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"} 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
