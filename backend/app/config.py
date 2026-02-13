36d6a14b8d7e3245c762687d358620bb5546da1a
misha <medinskijmisa228@gmail.com>
2026-02-13 11:37:13 +0100

    refactor(config): simplify config, remove Redis and cache namespace settings

--- backend/app/config.py
+++ backend/app/config.py
@@ -11,32 +11,8 @@ class AuthJWT(BaseModel):
     public_key_path: Path = BASE_DIR / "certs/jwt-public.pem"
     algorithm: str = "RS256"
 
-
-class RedisDB(BaseModel):
-    cache: int = 0
-
-
-class CacheNameSpaces(BaseModel):
-    users: str = "users"
-    contracts: str = "contracts"
-    transports: str = "transports"
-    files: str = "files"
-    residence_permits: str = "residence_permits"
-
-
-class CacheConfig(BaseModel):
-    prefix: str = "cache"
-    namespaces: CacheNameSpaces = CacheNameSpaces()
-
-
-class RedisConfig(BaseModel):
-    url: str = "redis://lochost:6379"
-    db: RedisDB = RedisDB()
-
-    @property
-    def connection_string(self) -> str:
-        return f"{self.url}/{self.db.cache}"
-
+class CacheSettings(BaseModel):
+    age: int = 60 # seconds
 
 class BucketSettings(BaseModel):
     access_key: str = ""
@@ -63,8 +39,7 @@ class Settings(BaseSettings):
     debug: bool = False
     postgres_url: str
     auth_jwt: AuthJWT = AuthJWT()
-    cache_config: CacheConfig = CacheConfig()
-    redis_config: RedisConfig = RedisConfig()
+    cache_config: CacheSettings = CacheSettings()
     bucket_config: BucketSettings
     cors_config: CORSConfig = CORSConfig()
 
